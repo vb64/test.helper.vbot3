742f8c7f206893ab26c66a81d7ac0e55ab55a29a
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-05-14 20:04:44 +0400

    remove command start from init

--- tester_vbot3/__init__.py
+++ tester_vbot3/__init__.py
@@ -28,12 +28,6 @@ class MockRequests:
         self.calls[url] += 1
 
 
-def start_command_dflt(message, bot):
-    """Handle private /start command."""
-    user = bot.user_entry_point(message)
-    bot.reply(message, "Hi ID {}".format(user.key.id()))
-
-
 class Vbot3Tester(TestFlask, TestGae3, Telemulator):
     """Tester for Vbot3."""
 
@@ -41,20 +35,15 @@ class Vbot3Tester(TestFlask, TestGae3, Telemulator):
     private = None
     group = None
 
-    def init(self, flask_app, vbot3, bot_name, bot_username, queue_yaml_dir, start_command_cust=None):
+    def init(self, flask_app, vbot3, bot_name, bot_username, queue_yaml_dir):
         """Init tests stuff."""
         TestFlask.set_up(self, flask_app)
         TestGae3.set_up(self, queue_yaml_dir)
         self.set_tested_bot(vbot3, name=bot_name, username=bot_username)
 
-        start_command = start_command_cust or start_command_dflt
-        vbot3.private_command('start')(lambda msg: start_command(msg, vbot3))
-        vbot3.set_default_content_handlers()  # must be last in handlers chain
-
         self.teleuser = self.api.create_user('Test', 'User', language_code='en')
         self.private = self.teleuser.private()
         self.group = self.teleuser.create_group("Test group")
-        self.private_command('/start')
 
     def tearDown(self):
         """Clear tests."""
